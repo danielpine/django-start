c96347b715008a41716397bd2f6366355dcbaeeb
danielpine <danielpine@sina.com>
2018-11-13 09:57:36 +0800

    update project name.

--- djangostart/mysite/urls.py
+++ djangostart/mysite/urls.py
@@ -1,5 +1,5 @@
 # -*-coding:utf-8 -*-
-"""djangostart URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
